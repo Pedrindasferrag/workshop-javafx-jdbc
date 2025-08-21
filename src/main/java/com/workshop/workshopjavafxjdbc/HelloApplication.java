4e63775bdc10fdd1362a3a250d324f9985b1d13d
Pedro Lima <pedrodavi951@gmail.com>
2025-08-21 20:34:36 -0300

    MainView.fxml creation

--- src/main/java/com/workshop/workshopjavafxjdbc/HelloApplication.java
+++ src/main/java/com/workshop/workshopjavafxjdbc/HelloApplication.java
@@ -2,6 +2,7 @@ package com.workshop.workshopjavafxjdbc;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -9,12 +10,18 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage){
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MainView.fxml"));
+            Parent parent = fxmlLoader.load();
+            Scene scene = new Scene(parent, 450, 350);
+            stage.setTitle("Sample JavaFX Application!");
+            stage.setScene(scene);
+            stage.show();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
     }
 
     public static void main(String[] args) {
